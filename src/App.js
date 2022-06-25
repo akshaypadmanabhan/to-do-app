5fcb6f498b1e58a008f1af9cd28aa9a542df95a0
Akshay Padmanabhan <akshayplearning@gmail.com>
2022-06-25 23:31:49 +0530

    Final File

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDos,setToDos]=useState([]);
+  const [toDo,setToDo]=useState('');
   return (
     <div className="app">
       <div className="mainHeading">
@@ -9,22 +12,54 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop,What You Want to do Today🌝 ☕ </h2>
+        <h2>Whoop,What You Want to do Today :-) </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(event)=>setToDo(event.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=> setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
+        {toDos.map((newValue)=>{
+          return(
+            <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>React tutorial</p>
+            <input onChange={(e)=>{
+              console.log(e.target.checked)
+              console.log(newValue)
+              setToDos(toDos.filter(newValue2=>{
+                if(newValue2.id===newValue.id){
+                  newValue2.status=e.target.checked;
+                  return newValue2;
+                }return null;
+              }))
+
+            }} value={newValue.status} type="checkbox" name="" id="" />
+            <p>{newValue.text}</p>
           </div>
           <div className="right">
+             <div>{toDos.map((newValue)=>{
+          if(newValue.status){
+            return(<h3>Active Task</h3>)
+          }return null;
+        })}
+        </div>
+        <div className='delete'>
             <i className="fas fa-times"></i>
+         </div>   
           </div>
         </div>
+
+
+
+          )
+        })
+
+
+
+
+
+        }
+        
       </div>
     </div>
   );
